2d4d47eecb74b0b1eee24b54b6458a5fe2dd9900
marketingwithdata <prgray86@gmail.com>
2021-08-13 15:27:33 +0100

    Create trader.py

---
+++ trader.py
@@ -0,0 +1,47 @@
+import backtrader as bt
+import datetime
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+
+from strategies import TestStrategy
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
+
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, 'ORCL.csv')
+
+    # Create a Data Feed
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath,
+        # Do not pass values before this date
+        fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values before this date
+        todate=datetime.datetime(2000, 12, 31),
+        # Do not pass values after this date
+        reverse=False)
+
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    cerebro.broker.setcash(100.0)
+    cerebro.broker.setcommission(commission=0.001)
+
+    # Set our desired cash start
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.plot()
